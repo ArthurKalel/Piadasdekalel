c14b628556d1e7b643cd8fb0e39254e4a86e5ec9
ArthurKalel <ArthurKalel@vivaldi.net>
2024-11-21 22:30:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
+function pesquisa() {
+  var p = document.getElementById("pesquisa")
 
-
-function   abremenu() {
-  var n = document.getElementById("nav");
-
-  if(n.style.display=="flex")  {
-    n.style.display="none";
-  }else {
-    n.style.display="flex";
+  if (p.style.display == "flex") {
+    p.style.display = "none"
+  } else {
+    p.style.display = "flex"
   }
-  
 }
 
 
-function   fechamenu() {
-    var f =  document.getElementById("nav");
+function abremenu() {
+  var n = document.getElementById("nav");
+  var e = document.getElementById("english")
+  if (n.style.display == "flex") {
+    n.style.display = "none";
+    e.style.display = "none";
 
-    if(f.style.display=="flex")  {
-      f.style.display="none";
-    }else {
-      f.style.display="flex";
-    }
-    
+  } else {
+    n.style.display = "flex";
+    e.style.display = "flex";
+  }
+
+}
+
+
+function fechamenu() {
+  var f = document.getElementById("nav");
+
+  if (f.style.display == "flex") {
+    f.style.display = "none";
+  } else {
+    f.style.display = "flex";
+  }
+
+}
+
+function loadPage(page) {
+  document.getElementById('content-frame').src = page;
 }
